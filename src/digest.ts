219fd35b6aebc272a88dc71467f4a454b4d65664
Vladimir Ilyin <vladimir.ilyin@neueda.com>
2019-01-14 11:46:43 +0200

    Remove exclusions by file extension, sign all files in the app dir

--- src/digest.ts
+++ src/digest.ts
@@ -2,12 +2,10 @@ import {hashElement} from 'folder-hash';
 
 const hashAlg = 'blake2b512';
 
-const excludeDefault = ['*.svg'];
-
 export const digestDirectory = async (path: string, excludeFiles: string[]): Promise<string> => {
     const result = await hashElement(path, {
         algo: hashAlg,
-        files: {exclude: [...excludeFiles, ...excludeDefault]},
+        files: {exclude: [...excludeFiles]},
         folders: {ignoreRootName: true}
     });
     return Promise.resolve(result.hash);
