9e91ef99abed35185090378c8b5546a71d43e599
Hugo Bove <hugo.bove@neotechnology.com>
2020-02-28 12:45:20 +0100

    correct URL

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,7 @@ import {pki} from "node-forge";
 
 export const DIGEST_ALGORITHM_OID = pki.oids.sha256;
 export const SIGNATURE_FILENAME = 'signature.pem';
-export const CERTIFICATION_SERVER_PUBLIC_KEY = '';
-export const CERTIFICATION_SERVER_URL = 'http://localhost:3000/verify';
+export const CERTIFICATION_SERVER_PUBLIC_KEY = `
+@todo: add verification server public cert
+`;
+export const CERTIFICATION_SERVER_URL = 'https://trust.neo4j.com/api/v1/verify/app/signature';
